58979604c0368be57dd0ca75d17e744316f52b4a
Anisha Gupta <41622775+Anishaagupta@users.noreply.github.com>
2023-04-23 23:00:01 +0530

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
           <article className="contact__option">
             <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
-            <h5>anisha8057@gmail.com</h5>
+            <h5>anishagupta8057@gmail.com</h5>
             <a href="mailto:anishagupta8057@gmail.com">Send a message</a>
           </article>
         </div>
